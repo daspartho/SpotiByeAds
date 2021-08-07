0596330b472cfbf5d5fd6f8ae0c2c7afe8040f11
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-08-07 10:42:25 -0400

    Add requirements.txt, dynamic spotify detection, and Linux support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SpotiByeAds
 
-No one likes interruptions! right? So this program detects when an advertisement plays by monitoring the type of the track that is currently playing, using the Spotipy API. 
+No one likes interruptions! right? So this program detects when an advertisement plays by monitoring the type of the track that is currently playing, using the Spotipy API.
 When an ad is detected, the program restarts Spotify by the os module and plays it via pynput, which skips the ad and starts right where you left off.
 
 ### Installation
@@ -11,10 +11,7 @@ git clone https://github.com/daspartho/SpotiByeAds.git
 cd SpotiByeAds.py
 ```
 ```
-pip install pynput
-```
-```
-pip install spotipy
+pip install -r requirements.txt
 ```
 
 ### Setting up
@@ -23,7 +20,7 @@ pip install spotipy
 2. Click on the 'CREATE AN APP' option and provide an app name and app description as you'd like.
 3. Go to 'EDIT SETTINGS' and fill in the Redirect URIs placeholder with http://localhost:8080/, and click on Save.
 4. Copy the Client ID and Client Secret and paste it in the corresponding place holders in main.py.
-5. Paste the path to the spotify application on your computer in the PATH placeholder in main.py.
+5. Spotify's path with be automatically detected, but if you would like to override it or if the auto detection fails, paste the path to the spotify application on your computer in the PATH placeholder in main.py.
 6. Paste your spotify username in the username placeholder in main.py
 
 ### Usage
--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 import time
 import os
+import shutil
+import subprocess
+
 import spotipy
 from spotipy import util
 from pynput.keyboard import Key, Controller
 
 def closeSpotify():
-    os.system("taskkill /f /im spotify.exe")
+    if os.name == 'nt':
+        # windows
+        os.system("taskkill /f /im spotify.exe")
+    else:
+        # most everything else
+        os.system('killall -9 spotify')
 
 def openSpotify(path):
-    os.startfile(path)
+    if not path:
+        path = shutil.which('spotify')
+
+    subprocess.Popen([path], start_new_session=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 def playSpotify():
     keyboard = Controller()
     keyboard.press(Key.media_play_pause)
     keyboard.release(Key.media_play_pause)
-    
+
 def previousWindow():
     keyboard = Controller()
     keyboard.press(Key.alt_l)
     keyboard.press(Key.tab)
     keyboard.release(Key.alt_l)
     keyboard.release(Key.tab)
-    
+
 def restartSpotify(path):
     closeSpotify()
     openSpotify(path)
@@ -33,30 +44,32 @@ def setupSpotifyObject(username, scope, clientID, clientSecret, redirectURI):
     token = util.prompt_for_user_token(username, scope, clientID, clientSecret, redirectURI)
     return spotipy.Spotify(auth=token)
 
-def main(username, scope, clientID, clientSecret, redirectURI, path):    
+def main(username, scope, clientID, clientSecret, redirectURI, path):
     spotify = setupSpotifyObject(username, scope, clientID, clientSecret, redirectURI)
 
+    restartSpotify(path)
+
     while True:
-        
+
         try:
             current_track = spotify.current_user_playing_track()
-        except:
+        except Exception:
             print('token expired')
             spotify = setupSpotifyObject(username, scope, clientID, clientSecret, redirectURI)
             current_track = spotify.current_user_playing_track()
-            
+
         try:
             if current_track['currently_playing_type'] == 'ad':
                 restartSpotify(path)
                 print('Ad skipped')
         except TypeError:
             pass
-        
+
         time.sleep(1)
 
 if __name__ == '__main__':
-    
-    PATH = ''
+
+    PATH = ""
     spotifyUsername = ""
     spotifyClientID = ""
     spotifyClientSecret = ""
