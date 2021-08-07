88986d2d48f52c8153bf43aba9a45a9bb8dfdfbb
mmr <binary@b1n.org>
2021-08-07 20:42:01 +0200

    Reading/writing config from/to config file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import time
 import os
+import configparser
+
 import spotipy
 from spotipy import util
 from pynput.keyboard import Key, Controller
+
+CONFIG_FILE = 'config.properties'
+CONFIG_BASE = 'spotify'
 
 def closeSpotify():
     os.system("taskkill /f /im spotify.exe")
@@ -15,7 +20,7 @@ def playSpotify():
     keyboard.press(Key.media_play_pause)
     keyboard.release(Key.media_play_pause)
     
-def previousWindow():
+def setupSpotifyObject():
     keyboard = Controller()
     keyboard.press(Key.alt_l)
     keyboard.press(Key.tab)
@@ -29,39 +34,60 @@ def restartSpotify(path):
     playSpotify()
     previousWindow()
 
-def setupSpotifyObject(username, scope, clientID, clientSecret, redirectURI):
-    token = util.prompt_for_user_token(username, scope, clientID, clientSecret, redirectURI)
+def setupSpotifyObject(config):
+    token = util.prompt_for_user_token(
+            config.get(CONFIG_BASE, 'username'),
+            config.get(CONFIG_BASE, 'acessScope'),
+            config.get(CONFIG_BASE, 'clientID'),
+            config.get(CONFIG_BASE, 'clientSecret'),
+            config.get(CONFIG_BASE, 'redirectURI'))
     return spotipy.Spotify(auth=token)
 
-def main(username, scope, clientID, clientSecret, redirectURI, path):    
-    spotify = setupSpotifyObject(username, scope, clientID, clientSecret, redirectURI)
+def create_config_file():
+    config = configparser.ConfigParser()
+
+    config[CONFIG_BASE] = {
+        'accessScope': 'user-read-currently-playing',
+        'redirectURI': 'http://localhost:8080/',
+        'path': ''
+    }
+
+    for n in ('username', 'clientID', 'clientSecret'):
+        config[CONFIG_BASE][n] = input(f'Enter Spotify {n}: ')
+
+    with open(CONFIG_FILE, 'w') as cf:
+        config.write(cf)
+
+    print(f'Config file {CONFIG_FILE} created. Remove it to reconfigure.')
+
+def load_config_file():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    return config
+
+def main():
+    config = load_config_file()
+    spotify = setupSpotifyObject(config)
 
     while True:
-        
+
         try:
             current_track = spotify.current_user_playing_track()
         except:
             print('token expired')
-            spotify = setupSpotifyObject(username, scope, clientID, clientSecret, redirectURI)
+            spotify = setupSpotifyObject(config)
             current_track = spotify.current_user_playing_track()
-            
+
         try:
             if current_track['currently_playing_type'] == 'ad':
                 restartSpotify(path)
                 print('Ad skipped')
         except TypeError:
             pass
-        
+
         time.sleep(1)
 
 if __name__ == '__main__':
-    
-    PATH = ''
-    spotifyUsername = ""
-    spotifyClientID = ""
-    spotifyClientSecret = ""
-    spotifyAccessScope = "user-read-currently-playing"
-    spotifyRedirectURI = "http://localhost:8080/"
-
-    main(spotifyUsername, spotifyAccessScope, spotifyClientID, spotifyClientSecret, spotifyRedirectURI, PATH)
-
+    if not os.path.isfile(CONFIG_FILE):
+        create_config_file()
+    main()
