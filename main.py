03adc934dd382ab49a0cd1736e293f0825ef7451
The-Glit-ch <matthewflorenzano1@gmail.com>
2021-08-09 14:34:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
-#NOTE: Most of the other fixes dynamic spotify support came from https://github.com/daspartho/SpotiByeAds/pull/3
+import sys, os, time, shutil, subprocess, json  # Base Libs
 
-while True: #If we get no import errors then break from the loop. If we do get an error install the dependencies and do the try/catch block again.
-    try:
-        import os,time,shutil,subprocess #Base Libs
-        import spotipy
-        from spotipy import util, SpotifyException
-        from pynput.keyboard import Key, Controller
-        break
-    except ImportError:
-        print("Import Error: Downloading off 'requirements.txt'")
-        time.sleep(1)
-        os.system("pip3 install -r requirements.txt")
-        print("Done!")
+
+try:
+    import spotipy
+    from pynput.keyboard import Key, Controller
+except ImportError:
+    print("DEPENDENCIES NOT INSTALLED!"
+          "\nPlease install the depenencies by running:"
+          "\n\t`python -m pip install -r requirements.txt`")
+    sys.exit(1)
 
 #Vars
-keyboard = Controller() #I noticed how we kept making a new keyboard controller instance so decided to just make it a variable
+keyboard = Controller()
+SubtractDelay = 6 #look at ln 73
 
 def closeSpotify():
-    if os.name == "nt":
-        os.system("taskkill /f /im spotify.exe") #Windows
-    else:
-        os.system("killall -9 spotify") #Other
+    if os.name == "nt":  # windows
+        os.system("taskkill /f /im spotify.exe")
+    elif sys.platform == "darwin":  # Mac OS
+        # Not exactly sure of the process name, so used regex.
+        os.system("killall -9 -r [Ss]potify.*")
+    else:  # almost everything else
+        os.system("killall -9 spotify")
 
-def openSpotify(path:str):
-    if not path: #If we did not provide a value for path
-        path = shutil.which("spotify")
-    
-    subprocess.Popen([path], start_new_session=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+def openSpotify(path):
+    subprocess.Popen([path], start_new_session=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
 
 def playSpotify():
-    keyboard.press(Key.media_play_pause)
-    keyboard.release(Key.media_play_pause)
+    keyboard.press(Key.media_next)
+    keyboard.release(Key.media_next)
     
 def previousWindow():
     keyboard.press(Key.alt_l)
@@ -38,7 +36,7 @@ def previousWindow():
     keyboard.release(Key.alt_l)
     keyboard.release(Key.tab)
     
-def restartSpotify(path:str):
+def restartSpotify(path):
     closeSpotify()
     openSpotify(path)
     time.sleep(5)
@@ -46,38 +44,96 @@ def restartSpotify(path:str):
     previousWindow()
 
 def setupSpotifyObject(username, scope, clientID, clientSecret, redirectURI):
-    try:
-        token = util.prompt_for_user_token(username, scope, clientID, clientSecret, redirectURI)
-        return spotipy.Spotify(auth=token)
-    except Exception: #I dont feel like reading the docs
-        exit("Error: Invalid/Incorrect Credentials")
+    token = spotipy.util.prompt_for_user_token(username, scope, clientID,
+                                               clientSecret, redirectURI)
+    return spotipy.Spotify(auth=token)
 
 def main(username, scope, clientID, clientSecret, redirectURI, path):    
     spotify = setupSpotifyObject(username, scope, clientID, clientSecret, redirectURI)
+
+    print("\nAwesome, that's all I needed. I'm watching for ads now <.<")
     restartSpotify(path)
 
     while True:
-        try:
-            if spotify.current_user_playing_track()['currently_playing_type'] == "ad":
-                restartSpotify(path)
-                print("Ad Skipped")
-        except SpotifyException: #I checked the docs and it only throws "spotipy.client.SpotifyException"...weird
-            print("Token Expired")
-            spotify = setupSpotifyObject(username, scope, clientID, clientSecret, redirectURI)
         
-
-        time.sleep(spotify.current_user_playing_track['duration_ms']/1000) 
+        try:
+            current_track = spotify.current_user_playing_track()
+        except spotipy.SpotifyException:
+            print('Token expired')
+            spotify = setupSpotifyObject(username, scope, clientID, clientSecret, redirectURI)
+            current_track = spotify.current_user_playing_track()
+            
+        try:
+            if current_track['currently_playing_type'] == 'ad':
+                restartSpotify(path)
+                print('Ad skipped')
+        except TypeError:
+            pass
+        
+        time.sleep((spotify.current_user_playing_track['duration_ms']/1000) - SubtractDelay) #Stop us from getting rate limited from spotify API
         #Instead of checking every second for an ad. We simply get the total length of the song and wait for that ammount of time. 
         #Now spotify returns miliseconds for song length which is weird but this can easily be fixed with some basic math.
         #We get the song duration and divide it by 1000 and we get our time in seconds
 
 if __name__ == '__main__':
-    PATH = ""
-    spotifyUsername = ""
-    spotifyClientID = ""
-    spotifyClientSecret = ""
+    # these are kinda constants
+    PATH = (shutil.which("spotify")  # For any system with spotify on $PATH
+            or ("{HOMEDRIVE}{HOMEPATH}\AppData\Roaming\Spotify\Spotify.exe"
+                .format_map(os.environ) if os.name == "nt"  # Windows
+                else "/Applications/Spotify.app" if sys.platform == "darwin"  # MacOS
+                else ""  # Popen expects a path-like object, `None` results in an error.
+               )
+           )
     spotifyAccessScope = "user-read-currently-playing"
     spotifyRedirectURI = "http://localhost:8080/"
 
-    main(spotifyUsername, spotifyAccessScope, spotifyClientID, spotifyClientSecret, spotifyRedirectURI, PATH)
+    try:
+        with open("./credentials.json", "r") as creds_json:
+            creds = json.load(creds_json)
 
+            load = input("Found previously used credentials."
+                         " Want to use them again? (Y/n) "
+                        ).lower()
+
+            if load != "y":
+                if load == "n":
+                    print("User didn't want to load from save.")
+                    raise FileNotFoundError
+                else:
+                    print("Unrecognized Input.")
+                    creds_json.close()  # The program exits immediately below.
+                    sys.exit(0)
+
+            spotify_username = creds["spotify_username"]
+            spotify_client_id = creds["spotify_client_id"]
+            spotify_client_secret = creds["spotify_client_secret"]
+
+    except FileNotFoundError:
+        spotify_username = input("Ok, what's your Spotify username? ")
+        spotify_client_id = input("Great, now what's the ClientID you're using? ")
+        spotify_client_secret = input("Beautiful, now how about the Client Secret? ")
+
+        save = input("Awesome, now would you like to save these settings"
+                     " for future sessions? (Y/n) "
+                    ).lower()
+        
+        if save == "y":
+            save_obj = {
+                "spotify_username": spotify_username,
+                "spotify_client_id": spotify_client_id,
+                "spotify_client_secret": spotify_client_secret
+            }
+
+            with open("./credentials.json", "w") as creds:
+                creds.write(json.dumps(save_obj))
+                creds.close()
+
+                print("Saved.")
+
+        elif save == "n":
+            print("Not saving settings.")
+
+        else:
+            print("Didn't recognize input, defaulted to not saving.")
+
+    main(spotify_username, spotifyAccessScope, spotify_client_id, spotify_client_secret, spotifyRedirectURI, PATH)
